a1174c4fa953dc13575e40f67c3f822da4eb66f2
florisvanderheijde <49977319+florisvanderheijde@users.noreply.github.com>
2021-10-19 11:58:00 +0200

    Create testing_keyb_gatt.ino

---
+++ Prototype code/Testing_Keyb_GATT/testing_keyb_gatt/testing_keyb_gatt.ino
@@ -0,0 +1,15 @@
+#include <BleKeyboardGATT.h>
+#include <GATTCallbacks.h>
+
+BleKeyboard bleKeyboard("Gamer-BUG","TU Delft");
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  bleKeyboard.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
