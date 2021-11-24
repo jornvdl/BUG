68bf5a42d5645518343cb45dad3dda2e5dd2ed90
Jorn vdL <jorn.v.d.l@gmail.com>
2021-11-24 16:58:11 +0100

    Added btn function

--- Final Code/ESP/ESP_v2-0/btn.h
+++ Final Code/ESP/ESP_v2-0/btn.h
@@ -1,6 +1,23 @@
+/* Button header
+ *  btnPress() is called when main detects a button press. It sends the event
+ *  to the library and waits until the button is released. It then sends that
+ *  event to the library and ends the function, returning to the main scope.
+ */
+
+
 #ifndef _BTN_H
 #define _BTN_H
 
 
+void btnPress() {
+  if (debug) Serial.println("Game button pressed!");
+
+  bleKeyboard.press();
+  while(digitalRead(btnPin)) {};
+  bleKeyboard.releaseAll();
+  // Sleeptimer reset
+
+  if (debug) Serial.println("Game button released!");
+}
 
 #endif // _BTN_H
