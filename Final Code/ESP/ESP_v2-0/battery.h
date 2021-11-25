e17c8025edd763fed4024e60ab27f3f687ed3dee
florisvanderheijde <49977319+florisvanderheijde@users.noreply.github.com>
2021-11-25 14:05:30 +0100

    Update battery.h

--- Final Code/ESP/ESP_v2-0/battery.h
+++ Final Code/ESP/ESP_v2-0/battery.h
@@ -3,10 +3,10 @@
 
 void batterySend() {
   int batmeasure[20];
-  for(m = 0; m < 20; m++) {
-    batmeasure[m] = analogRead(BatteryPin);
+  float total;
+  for(int m = 0; m < 20; m++) {
+    batmeasure[m] = analogRead(batPin);
     total = total + batmeasure[m];
-    m++;
   }
   float avg         = total/20;
   float bV          = 0.0021*avg - 0.4452;
@@ -16,9 +16,9 @@ void batterySend() {
   int batt5         = battRound/5;
   int battFin       = batt5*5;
 
-  bleKeyboard.setBatterLevel(battFin);
+  bleKeyboard.setBatteryLevel(battFin);
 
-  if (debug) Serial.println("Battery percentage calculated and sent")
+  if (debug) Serial.println("Battery percentage calculated and sent");
 }
 
 #endif // _BATTERY_H
